14faedece0fb0134afba2c67d3aae211254810a2
Johanna Alvarado Z <jp.alvaradozarate@gmail.com>
2016-08-25 17:49:53 -0500

    Costo de llamada - js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# costo-llamada-js
+EJERCICIO 4
+##Título: Costo llamada telefónica
+
+## Contexto:
+El costo total de una llamada telefónica determinada por su duración y el costo por minuto.
+##Pseudocódigo
+1. Inicio
+2. tiempo
+3. costoMinuto
+4.Multiplicar tiempo * costoMinuto
+5.resultado  15
+6.FIN
+
+##imagen
+![alt text](http://1.1m.yt/7QIrRfG.jpg "Imagen diagrama")
---
+++ js/app.js
@@ -0,0 +1,16 @@
+var duracionDeLlamada = prompt("Ingresa duración de la llamada");
+   	duracionDeLlamada = parseFloat(duracionDeLlamada);
+var costoPorMinuto= prompt("Ingresa costo por minuto");
+   	costoPorMinuto = parseFloat(costoPorMinuto);
+var costoDeLlamada = duracionDeLlamada*costoPorMinuto;
+document.write("Su costo total de su llamada es:");
+document.write(costoDeLlamada);
+
+/*
+<--Otra forma-->
+
+var num1 = prompt ("Duración de la llamada");
+var num2 = prompt ("Costo por minuto");
+document.write("El costo de la llamada es: ");
+document.write(parseInt(num1)*parseInt(num2));
+*/
